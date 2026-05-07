5ce732d6c6dfde9de17b56dab9a9f82be14eb116
Fluished <chronobait@gmail.com>
2026-05-07 18:05:14 +0800

    feat: show wallet on navbar

---
+++ Shelflyx/ViewComponent/WalletViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shelflyx.Data;
+using System.Security.Claims;
+
+public class WalletViewComponent : ViewComponent
+{
+    private readonly AppDbContext _context;
+
+    public WalletViewComponent(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        decimal balance = 0;
+
+        var userId = UserClaimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (int.TryParse(userId, out int id))
+        {
+            var user = await _context.Users.FindAsync(id);
+            balance = user?.Balance ?? 0;
+        }
+
+        return View(balance);
+    }
+}
